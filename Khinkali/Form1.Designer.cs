a218d1d6f5b7ffd6ab9c92761b6e48ee4e6819aa
khechoshvili <shalvakhechoshvili7@gmail.com>
2026-04-06 13:24:57 -0700

    Initial commit

---
+++ Khinkali/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Khinkali
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblScore = new System.Windows.Forms.Label();
+            this.GameTimer = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.Khinkali = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Khinkali)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblScore.Location = new System.Drawing.Point(109, 20);
+            this.lblScore.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(71, 20);
+            this.lblScore.TabIndex = 5;
+            this.lblScore.Text = "Score : ";
+            // 
+            // GameTimer
+            // 
+            this.GameTimer.Interval = 20;
+            this.GameTimer.Tick += new System.EventHandler(this.GameTimerEvent);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Location = new System.Drawing.Point(-9, 467);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(876, 51);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.Location = new System.Drawing.Point(396, 402);
+            this.pictureBox5.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(26, 74);
+            this.pictureBox5.TabIndex = 6;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Tag = "obstacle";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::Khinkali.Properties.Resources.knife;
+            this.pictureBox4.Location = new System.Drawing.Point(735, 402);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(24, 74);
+            this.pictureBox4.TabIndex = 7;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Tag = "obstacle";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::Khinkali.Properties.Resources.forkKnife;
+            this.pictureBox3.Location = new System.Drawing.Point(565, 402);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(43, 74);
+            this.pictureBox3.TabIndex = 8;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Tag = "obstacle";
+            // 
+            // Khinkali
+            // 
+            this.Khinkali.Image = global::Khinkali.Properties.Resources.KhinkaliRunning;
+            this.Khinkali.Location = new System.Drawing.Point(152, 385);
+            this.Khinkali.Margin = new System.Windows.Forms.Padding(2);
+            this.Khinkali.Name = "Khinkali";
+            this.Khinkali.Size = new System.Drawing.Size(74, 78);
+            this.Khinkali.TabIndex = 9;
+            this.Khinkali.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.ClientSize = new System.Drawing.Size(864, 510);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.Khinkali);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Tag = "obstacle";
+            this.Text = "KhinkaliGame";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.GameKeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.GameKeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Khinkali)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox Khinkali;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Timer GameTimer;
+    }
+}
+
---
+++ Khinkali/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+namespace Khinkali
+{
+    public partial class Form1 : Form
+    {
+        int score = 0;
+        bool isJumping = false;
+        bool gameOver = false;
+
+        int verticalSpeed = 0;
+        int obstacleSpeed = 8;
+
+        int groundY = 385;
+
+        bool counted3 = false;
+        bool counted4 = false;
+        bool counted5 = false;
+
+
+        Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.KeyPreview = true;
+            this.KeyDown += GameKeyDown;
+            this.KeyUp += GameKeyUp;
+
+            GameReset();
+            GameTimer.Start();
+        }
+
+
+        private void GameTimerEvent(object sender, EventArgs e)
+        {
+
+            if (gameOver) return;
+
+            lblScore.Text = "Score: " + score;
+
+            if (isJumping)
+            {
+                Khinkali.Top += verticalSpeed;
+                verticalSpeed += 1;
+
+                if (Khinkali.Top >= groundY)
+                {
+                    Khinkali.Top = groundY;
+                    isJumping = false;
+                    verticalSpeed = 0;
+                  
+                }
+            }
+
+            foreach (Control x in this.Controls)
+            {
+                if (x is PictureBox && x != Khinkali && x.Tag != null && x.Tag.ToString() == "obstacle")
+                {
+                    x.Left -= obstacleSpeed;
+
+                    Rectangle playerHitBox = new Rectangle(
+                        Khinkali.Left + 15,
+                        Khinkali.Top + 10,
+                        Khinkali.Width - 30,
+                        Khinkali.Height - 10
+                    );
+
+                    Rectangle obstacleHitBox = new Rectangle(
+                        x.Left + 5,
+                        x.Top + 5,
+                        x.Width - 10,
+                        x.Height - 10
+                    );
+
+                    if (playerHitBox.IntersectsWith(obstacleHitBox))
+                    {
+                        gameOver = true;
+                        GameTimer.Stop();
+                        lblScore.Text = "Score: " + score + "   Game Over";
+                    }
+
+                    if (x == pictureBox3 && !counted3 && x.Right < Khinkali.Left)
+                    {
+                        score++;
+                        counted3 = true;
+                    }
+
+                    if (x == pictureBox4 && !counted4 && x.Right < Khinkali.Left)
+                    {
+                        score++;
+                        counted4 = true;
+                    }
+
+                    if (x == pictureBox5 && !counted5 && x.Right < Khinkali.Left)
+                    {
+                        score++;
+                        counted5 = true;
+                    }
+
+                    if (x.Left < -100)
+                    {
+                        int farthestX = Math.Max(pictureBox3.Left, Math.Max(pictureBox4.Left, pictureBox5.Left));
+                        x.Left = farthestX + random.Next(300, 500);
+
+                        if (x == pictureBox3) counted3 = false;
+                        if (x == pictureBox4) counted4 = false;
+                        if (x == pictureBox5) counted5 = false;
+                    }
+                }
+            }
+        }
+
+
+
+        private void GameKeyDown(object sender, KeyEventArgs e)
+        {
+            if (gameOver && e.KeyCode == Keys.Enter)
+            {
+                GameReset();
+                GameTimer.Start();
+                return;
+            }
+
+            if (e.KeyCode == Keys.Space && !isJumping)
+            {
+                isJumping = true;
+                verticalSpeed = -18;
+            }
+        }
+
+        private void GameKeyUp(object sender, KeyEventArgs e)
+        {
+
+        }
+
+        private void GameReset()
+        {
+            score = 0;
+            isJumping = false;
+            gameOver = false;
+            verticalSpeed = 0;
+            
+            obstacleSpeed = 8;
+
+            lblScore.Text = "Score: " + score;
+            Khinkali.Image = Properties.Resources.KhinkaliRunning;
+
+            Khinkali.Top = groundY;
+            Khinkali.Left = 186;
+
+            pictureBox5.Left = 600;
+            pictureBox3.Left = 950;
+            pictureBox4.Left = 1300;
+
+
+            pictureBox5.Tag = "obstacle";
+            pictureBox3.Tag = "obstacle";
+            pictureBox4.Tag = "obstacle";
+
+
+            GameTimer.Start();
+        }
+
+      
+    }
+}
+
